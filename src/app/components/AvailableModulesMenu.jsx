73c6e9610532745316b573885217e66393e3b17b
willyb321 <wbwilliam7@gmail.com>
2017-10-12 15:44:56 +1100

    also it was set to tabs

--- src/app/components/AvailableModulesMenu.jsx
+++ src/app/components/AvailableModulesMenu.jsx
@@ -149,6 +149,7 @@ export default class AvailableModulesMenu extends TranslatedComponent {
         const existing = catmodules[moduleCategory] || [];
         catmodules[moduleCategory] = existing.concat(modules[g]);
       }
+
       for (let category in catmodules) {
         let categoryHeader = false;
         // Order through CATEGORIES if present
